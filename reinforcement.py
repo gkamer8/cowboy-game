dbbff3742e0d5309cbf8a4d0455236f98f799b58
Gordon Kamer <gkamer@outlook.com>
2021-09-09 12:16:38 -0400

    reinforcement v1 done - too many 0s and 1s

--- reinforcement.py
+++ reinforcement.py
@@ -1,15 +1,16 @@
-from genetic import CPU
+from genetic import CPU, faceoff
 import numpy as np
 
 class Reinforced(CPU):
 
     def __init__(self, probs=None):
-        super().__init__(props=probs)
-        move_history = []  # List of tuples with (my_bullet_count, opponent_bullet_count, move)
+        super().__init__(probs=probs)
+        self.move_history = []  # List of tuples with (my_bullet_count, opponent_bullet_count, move)
     
     def get_move(self, player, cpu):
         choice = np.random.choice(['R', 'X', 'S'], p=[self.reload[cpu][player], self.shield[cpu][player], self.shoot[cpu][player]])
         self.move_history.append((cpu, player, choice))
+        return choice
 
     # Win is boolean set to true if it's reinforcing a win
     # Delta = change in choice probability (absolute percentage points)
@@ -20,11 +21,42 @@ class Reinforced(CPU):
             # Note: Never increase or decrease a zero
             if not win:
                 delta *= -1
+            if move_dicts[move][cpu][player] >= 1:
+                continue
 
-            adjustment = delta  # Change for other two choices
+            move_dicts[move][cpu][player] += delta
+            if move_dicts[move][cpu][player] >= 1:
+                move_dicts[move][cpu][player] = 1
+                inverse_dicts[move][0][cpu][player] = 0
+                inverse_dicts[move][1][cpu][player] = 0
+            elif move_dicts[move][cpu][player] <= 0:
+                move_dicts[move][cpu][player] = 0
+                inverse_dicts[move][0][cpu][player] = inverse_dicts[move][0][cpu][player] / (inverse_dicts[move][0][cpu][player] + inverse_dicts[move][1][cpu][player])
+                inverse_dicts[move][1][cpu][player] = 1 - inverse_dicts[move][0][cpu][player]
+            else:
+                inverse_dicts[move][0][cpu][player] = (1 - move_dicts[move][cpu][player]) * (inverse_dicts[move][0][cpu][player] / (inverse_dicts[move][0][cpu][player] + inverse_dicts[move][1][cpu][player]))
+                inverse_dicts[move][1][cpu][player] = 1 - move_dicts[move][cpu][player] - inverse_dicts[move][0][cpu][player]
+            """
+            adjustment = delta / 2  # Change for other two choices
             for inv in inverse_dicts[move]:
-                if inv[cpu, player] == 0:
-                    adjustment *= 2
+                if inv[cpu][player] - adjustment <= 0:
+                    adjustment = delta
                 else:
-                    inv[cpu, player] -= adjustment / 2
-            move_dicts[move] = min(move_dicts[move] + delta, 1)  # Can't go above one
+                    inv[cpu][player] -= adjustment
+
+            move_dicts[move][cpu][player] = 1 - inverse_dicts[move][0][cpu][player] - inverse_dicts[move][1][cpu][player]  # Can't go above one
+            """
+
+if __name__ == '__main__':
+    CPU1 = Reinforced()
+    CPU2 = Reinforced()
+    rounds = 500
+    speak = 200
+    for x in range(rounds):
+        if x % speak == 0:
+            print("Round: " + str(x))
+        result = faceoff(CPU1, CPU2)
+        CPU2.reinforce(result)
+        CPU1.reinforce(not result)
+    CPU1.show_probs()
+        
